cbe8d4f271158b4f4b87c3cc6066f3b628f07d0a
Mario-ID <125501273+Mario-ID@users.noreply.github.com>
2023-07-05 11:35:19 +0200

    Initial upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Messages from './Messages';
+import Input from './Input';
+import { generateName, randomColor } from './generateName';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    messages: [],
+    member: {
+      username: generateName(),
+      color: randomColor(),
+    },
+  };
+
+  constructor() {
+    super();
+    this.drone = new window.Scaledrone('v59BNEQoU3gxLDcr', {
+      data: this.state.member,
+    });
+    this.drone.on('open', (error) => {
+      if (error) {
+        return console.error(error);
+      }
+      const member = { ...this.state.member };
+      member.id = this.drone.clientId;
+      this.setState({ member });
+    });
+    const room = this.drone.subscribe('observable-room');
+    room.on('data', (data, member) => {
+      const messages = [...this.state.messages];
+      messages.push({ member, text: data });
+      this.setState({ messages });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="chatBox">
+          <div className="header">
+            <p>Chat App</p>
+          </div>
+          <Messages messages={this.state.messages} currentMember={this.state.member} />
+          <Input onSendMessage={this.onSendMessage} />
+        </div>
+      </div>
+    );
+  }
+  onSendMessage = (message) => {
+    this.drone.publish({
+      room: 'observable-room',
+      message,
+    });
+  };
 }
 
 export default App;
---
+++ src/Input.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+  state = {
+    text: '',
+  };
+
+  onChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({ text: '' });
+    this.props.onSendMessage(this.state.text);
+  }
+
+  render() {
+    return (
+      <div className="Input">
+        <form onSubmit={(e) => this.onSubmit(e)}>
+          <input
+            onChange={(e) => this.onChange(e)}
+            value={this.state.text}
+            type="text"
+            placeholder=" Messege"
+            autoFocus
+          />
+          <button className="btnSend">
+            <img src={require('./media/send.png')} alt="buttonpng" width={'30px'} />
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Input;
